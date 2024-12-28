7adb96dec8a0f6d2524535e17bad42852633edea
Ando <hi@andostronaut.com>
2024-12-28 14:26:46 +0300

    feat: set lazy to false and create mappings for copilot

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -465,4 +465,16 @@ M.gitsigns = {
   },
 }
 
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true }
+    }
+  }
+}
+
 return M
