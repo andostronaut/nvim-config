90d28c987d65ad99bc76740b59e563e7dc333227
Ando <hi@andostronaut.com>
2025-01-03 16:13:21 +0300

    fix: mapping key for accept copilot suggestion

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -467,14 +467,14 @@ M.gitsigns = {
 
 M.copilot = {
   i = {
-    ["<C-l>"] = {
+    ["<leader>a"] = {
       function()
-        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
       end,
       "Copilot Accept",
-      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true }
-    }
-  }
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+    },
+  },
 }
 
 return M
