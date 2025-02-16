76695d61014e3d20ef4a15657c2e7336c5d7ad17
Ando <hi@andostronaut.com>
2025-02-16 16:00:22 +0300

    develop (#63)
    * fix: remove keymapping for copilot cancel not recognized (#60)
    
    * feat: setup `tiny-glimmer` plugin (#61)
    
    * feat: move copilot kay mappings in config init copilot (#62)

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -465,23 +465,4 @@ M.gitsigns = {
   },
 }
 
-M.copilot = {
-  i = {
-    ["<leader>a"] = {
-      function()
-        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
-      end,
-      "Copilot Accept",
-      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
-    },
-    ["<leader>c"] = {
-      function()
-        vim.fn.feedkeys(vim.fn["copilot#Cancel"](), "")
-      end,
-      "Copilot Cancel",
-      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
-    },
-  },
-}
-
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -468,7 +468,7 @@ local default_plugins = {
     end,
   },
 
-  -- troube
+  -- trouble
   {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
@@ -507,6 +507,20 @@ local default_plugins = {
     },
   },
 
+  -- supermaven
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<leader>sa",
+          clear_suggestion = "<leader>sc",
+        },
+      }
+    end,
+  },
+
   -- copilot
   {
     "github/copilot.vim",
@@ -515,8 +529,12 @@ local default_plugins = {
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
       vim.g.copilot_tab_fallback = ""
-      -- The mapping is set to other key, see custom/lua/mappings
-      -- or run <leader>ch to see copilot mapping section
+      vim.api.nvim_set_keymap("i", "<leader>ca", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+      vim.api.nvim_set_keymap("i", "<leader>cc", "copilot#Dismiss()", { expr = true, silent = true })
+      vim.g.copilot_filetypes = {
+        ["*"] = true,
+        ["mdx"] = true,
+      }
     end,
   },
 
@@ -538,6 +556,13 @@ local default_plugins = {
     config = function()
       require("telescope").load_extension "lazygit"
     end,
+  },
+
+  -- tiny-glimmer
+  {
+    "rachartier/tiny-glimmer.nvim",
+    event = "VeryLazy",
+    opts = {},
   },
 }
 
